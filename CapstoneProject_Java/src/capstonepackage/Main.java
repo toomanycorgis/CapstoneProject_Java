6210b73ec6e68360c5ca1df0a9799f671002277c
toomanycorgis <aria.burton95@gmail.com>
2020-10-03 16:44:51 -0400

    inital commit to add project to Github
    Signed-off-by: toomanycorgis <aria.burton95@gmail.com>

---
+++ CapstoneProject_Java/src/capstonepackage/Main.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package capstonepackage;
+
+/**
+ * @author ariella burton
+ *
+ */
+public class Main {
+
+	/**
+	 * main class
+	 * currently a placeholder for the rest of our Java Capstone Project
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
