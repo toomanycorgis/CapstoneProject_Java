0945817274952b71dfdae9ce71008bbdd1de6a7f
jolenic <jennifer.olenic@gmail.com>
2020-10-27 14:34:55 -0400

    Changed insertNewUser to accept User object instead of strings

--- CapstoneProject_Java/src/DBTest.java
+++ CapstoneProject_Java/src/DBTest.java
@@ -2,6 +2,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import dao.AppDAO;
+import users.Teacher;
 import users.User;
 
 public class DBTest {
@@ -9,9 +10,10 @@ public class DBTest {
 	public static void main(String[] args) throws SQLException {
 
 		AppDAO appDAO = new AppDAO();
-		appDAO.insertNewUser("JohnDoe1", "Passw0rd", "John", "Doe", "jd@mail.com", "123", "1", "T");
-		appDAO.insertNewUser("JaneDoe1", "Passw0rd", "Jane", "Doe", "jd@mail.com", "123", "0", "S");
-		appDAO.insertNewUser("Admin1", "Passw0rd", "Bob", "Builder", "bob@mail.com", "123", "1", "A");
+		User u1 = new Teacher("T1", "Test", "Teacher", "email@aslkdj.com", "password!");
+		appDAO.insertNewUser(u1);
+//		appDAO.insertNewUser("JaneDoe2", "Passw0rd", "Jane", "Doe", "jd@mail.com", "123", "0", "S");
+//		appDAO.insertNewUser("Admin2", "Passw0rd", "Bob", "Builder", "bob@mail.com", "123", "1", "A");
 		ArrayList<User> users = appDAO.showAllClassUsers();
 		for (User user : users) {
 			System.out.println(user.getUserName() + ": " + user.getRole() + " Is verified: " + user.isVerified());
--- CapstoneProject_Java/src/dao/AppDAO.java
+++ CapstoneProject_Java/src/dao/AppDAO.java
@@ -17,7 +17,7 @@ public class AppDAO {
 	private Connection connection;
 	private PreparedStatement insertNewUser;
 	private PreparedStatement getUser;
-	private PreparedStatement updateUser;
+//	private PreparedStatement updateUser;
 
 //	`username` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
 //  `password` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -37,9 +37,9 @@ public class AppDAO {
 					+ "(username, password, firstName, lastName, email, verificationCode, isVerified, userType)" 
 					+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
 			getUser = connection.prepareStatement("SELECT * FROM user" + " WHERE userName LIKE ?");
-			updateUser = connection.prepareStatement("UPDATE users " 
-					+ "SET password=?, firstName=?, lastName=?, email=?, verificationCode=?, isVerified=?, userType=?" 
-					+ "WHERE userName=?");
+//			updateUser = connection.prepareStatement("UPDATE users " 
+//					+ "SET password=?, firstName=?, lastName=?, email=?, verificationCode=?, isVerified=?, userType=?" 
+//					+ "WHERE userName=?");
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.exit(1);
@@ -50,17 +50,16 @@ public class AppDAO {
 		return connection;
 	}
 
-	public int insertNewUser(String userName, String password, String firstName, String lastName,
-			String email, String verificationCode, String isVerified, String userType) {
+	public int insertNewUser(User u) {
 		try {
-			insertNewUser.setString(1, userName);
-			insertNewUser.setString(2, password);
-			insertNewUser.setString(3, firstName);
-			insertNewUser.setString(4, lastName);
-			insertNewUser.setString(5, email);
-			insertNewUser.setString(6, verificationCode);
-			insertNewUser.setString(7, isVerified);
-			insertNewUser.setString(8, userType);
+			insertNewUser.setString(1, u.userName);
+			insertNewUser.setString(2, u.getPassword());
+			insertNewUser.setString(3, u.getFirstName());
+			insertNewUser.setString(4, u.getLastName());
+			insertNewUser.setString(5, u.getEmail());
+			insertNewUser.setString(6, u.getVerificationCode());
+			insertNewUser.setBoolean(7, (u.isVerified()));
+			insertNewUser.setString(8, u.getRole());
 			return insertNewUser.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -78,23 +77,23 @@ public class AppDAO {
 		}
 	}
 
-	public int updateUser(String userName, String password, String firstName, String lastName,
-			String email, String verificationCode, String isVerified, String userType) {
-		try {
-			updateUser.setString(1, password);
-			updateUser.setString(2, firstName);
-			updateUser.setString(3, lastName);
-			updateUser.setString(4, email);
-			updateUser.setString(5, verificationCode);
-			updateUser.setString(6, isVerified);
-			updateUser.setString(7, userType);
-			updateUser.setString(8, userName);
-			return updateUser.executeUpdate();
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return 0;
-		}
-	}
+//	public int updateUser(String userName, String password, String firstName, String lastName,
+//			String email, String verificationCode, String isVerified, String userType) {
+//		try {
+//			updateUser.setString(1, password);
+//			updateUser.setString(2, firstName);
+//			updateUser.setString(3, lastName);
+//			updateUser.setString(4, email);
+//			updateUser.setString(5, verificationCode);
+//			updateUser.setString(6, isVerified);
+//			updateUser.setString(7, userType);
+//			updateUser.setString(8, userName);
+//			return updateUser.executeUpdate();
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//			return 0;
+//		}
+//	}
 
 	public ArrayList<User> showAllClassUsers() throws SQLException {
 		ArrayList<User> dbUsers = new ArrayList<>();
@@ -111,19 +110,19 @@ public class AppDAO {
 			boolean isVerified = resultSet.getBoolean("isVerified");
 			String role = resultSet.getString("userType");
 			
-			if (role == "S") {
+			if (role.equals("S")) {
 				User u = new Student(userName, firstName, lastName, email, password);
 				u.setVerificationCode(verificationCode);
 				u.setVerified(isVerified);
 				u.setRole(role);
 				dbUsers.add(u);
-			} else if (role == "T") {
+			} else if (role.equals("T")) {
 				User u = new Teacher(userName, firstName, lastName, email, password);
 				u.setVerificationCode(verificationCode);
 				u.setVerified(isVerified);
 				u.setRole(role);
 				dbUsers.add(u);
-			} else if (role == "A") {}
+			} else if (role.equals("A")) {}
 				User u = new Admin(userName, firstName, lastName, email, password);
 				u.setVerificationCode(verificationCode);
 				u.setVerified(isVerified);
