f4abb779a27510a51b9b2bffe86fba1b846b3f08
Geoff <clar0745@algonquinlive.ca>
2020-10-28 11:18:00 -0400

    Testing Again

---
+++ CapstoneProject_Java/src/capstonepackage/Teacher.java
@@ -0,0 +1,25 @@
+
+public class Teacher extends User {
+	
+	private int teacherID;
+
+	public Teacher(String userName, String firstName, String lastName, String email, String password) {
+		super(userName, firstName, lastName, email, password);
+		super.setRole("T");
+	}
+
+	public Teacher(String userName, String password, String email) {
+		super(userName, password, email);
+		super.setRole("T");
+	}
+
+	public int getTeacherID() {
+		return teacherID;
+	}
+
+	public void setTeacherID(int teacherID) {
+		this.teacherID = teacherID;
+	}
+	
+
+}
