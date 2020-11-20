8999b9d1c1cd6ef460b09b56690d6bb1b1044676
jolenic <jennifer.olenic@gmail.com>
2020-11-19 22:13:23 -0500

    Added javax.activation jar file
    /Theoretically/ this should solve the issue with finding the DataHandler
    class

--- src/DBTest.java
+++ src/DBTest.java
@@ -27,7 +27,7 @@ public class DBTest {
 		System.out.println(appDAO.userExists("kasjdhksdjfbidsjhf"));
 		
 		//test SendMail ---- enter your own email to test it out ------
-		SendMail.send("example@example.com", "test", "this is a test");
+		SendMail.send("jennifer.olenic@gmail.com", "test", "this is a test");
 	}
 
 }
